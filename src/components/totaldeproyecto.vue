feed0b851af3d46d7f1f4bfcf4b5c18aabc13580
Juandepom2 <77644488+Juandepom2@users.noreply.github.com>
2022-10-15 13:51:19 -0500

    Add files via upload

---
+++ src/components/totaldeproyecto.vue
@@ -0,0 +1,41 @@
+<template>
+  <button style="    float: right;"   onclick="location.href = location.href" type="button" class="btn btn-warning">Limpiar</button>
+
+  <h3>
+    Total de proyectos: {{ numeroProyectos }}<!--lo escribiste mal ya que tiene que coincidir con lo que declaras en el scrip-->
+   </h3>
+    <div class="table-responsive">
+<table class="table table-dark table-hover">
+<thead>
+<tr>
+<th>#</th>
+<th>proyecto</th>
+<th>tipo</th>
+<th>urgente</th>
+<th>completado</th>
+</tr>
+</thead>
+
+<tbody>
+<tr v-for="(proyecto,index) in proyectos" :key="index">
+<td>{{ index+1 }}</td>
+<td>{{ proyecto.proyecto }}</td>
+<td>{{proyecto.tipo }}</td>
+<td @click="CambiarEstado(index)" :class="proyecto.urgente ? 
+'bg-success' : 'bg-danger'">{{ proyecto.urgente ? "Si" : "No"}}</td>
+<td @click="Cambiar(index)" :class="proyecto.completado ? 
+'bg-success' : 'bg-danger'">{{ proyecto.completado ? "Completo" : "Incompleto"}}</td> 
+</tr>
+</tbody>
+</table>
+</div>
+</template> 
+ 
+ <script>
+
+    export default{
+      props: ['numeroProyectos', 'proyectos', 'CambiarEstado', 'Cambiar'],
+        } 
+      </script>
+      <style>
+    </style>
